794155802b3306505ac8d4cf82bc43ae13af90a7
Nekit <nick.misiura@yandex.ru>
2024-07-02 10:49:05 +0300

    Change auto-scrolling to event listener, clean dead code, more comments

--- console/script.js
+++ console/script.js
@@ -9,30 +9,19 @@ const consoleLog = console.log;
 
 
 /// states
-// все убрать и сделать по-другому
-let isMultiline = false;
+let consoleScrolledDown = true;
 let isLogging = true;
-let scrollSwitch = false;
+let isMultiline = false; //сделать isSingleLine
 
 
 /// functions
-
+console.log = interseptConsoleLog;
 function interseptConsoleLog(msg) {
-    //скролл поменять
-    let isScrolledDown = getScrollingState(); // сразу, чтобы знать, что прокручено до конца
     consoleLog(msg);
     consoleOutput.innerHTML += `<p>${msg}</p>`;
-    scroll(isScrolledDown);
-}
-console.log = interseptConsoleLog;
-
-function bindConsole(console) {
-    console.log = interseptConsoleLog;
-};
-
-function test() {
-    console.log("test console");
-    return "test";
+    if (consoleScrolledDown) { // проскроллить вниз автоматически, если уже было внизу
+        consoleOutput.scroll(consoleOutput.scrollLeft, consoleOutput.scrollHeight);
+    }
 }
 
 function execute (event) {
@@ -43,37 +32,29 @@ function execute (event) {
     } catch(e) {
         console.log(e);
     }
-
-}
-
-//поменять на listener
-function scroll(isScrolledDown) {
-    const scrollTop = isScrolledDown 
-    ? consoleOutput.scrollHeight
-    : consoleOutput.scrollTop;
-    const scrollLeft = consoleOutput.scrollLeft;
-    consoleOutput.scroll(scrollLeft, scrollTop);
-}
-
-function getScrollingState() {
-    return (consoleOutput.scrollHeight - (consoleOutput.scrollTop + consoleOutput.clientHeight) ) <= 2;
 }
 
 
 /// events
+    //чек когда прокручено вниз
+consoleOutput.addEventListener("scroll", (e) => {
+    const element = e.currentTarget;
+    const posY = element.scrollHeight - (element.scrollTop + element.clientHeight);
+    consoleScrolledDown = (posY <= 5); // погрешность 5п на всякий
+});
 
-//get message from main window 
-//переделать на экспорт функции модуля
-//div вместо iframe, менять размер, сохраниение позиции
+    //get message from main window 
+    //переделать на экспорт функции модуля
+    //div вместо iframe, менять размер, сохраниение позиции
 window.addEventListener("message", (event) => {
     interseptConsoleLog(event.data);
 });
 
-// super bad idea
-// вместо этого добавлять и убирать listeners с разными лямбдами
+    // super bad idea
+    // вместо этого добавлять и убирать listeners с разными лямбдами?
 let ctrlPressed = false;
 let enterPressed = false;
-//
+
 consoleInput.addEventListener("keyup", (event) => {
     if (event.key === "Enter" && isMultiline) {
         enterPressed = true;
@@ -89,7 +70,7 @@ consoleInput.addEventListener("keyup", (event) => {
     ctrlPressed = false;
     enterPressed = false;
 });
-//
+
 consoleInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter" && !isMultiline) {
         event.preventDefault();
@@ -103,13 +84,26 @@ consoleInput.addEventListener("keydown", (event) => {
     
 });
 
-// clean console
+toggleConsoleMultiline.addEventListener("click", () => {
+    if (isMultiline) {
+        isMultiline = false;
+        toggleConsoleMultiline.textContent = "multiline input";
+        consoleInput.placeholder = "Enter";
+    } else if (!isMultiline) {
+        isMultiline = true;
+        toggleConsoleMultiline.textContent = "single line input";
+        consoleInput.placeholder = "Ctrl + Enter";
+    }
+    
+});
+
+    // clean console, все ок
 clearConsoleBtn.addEventListener("click", () => {
     consoleOutput.innerHTML = "";
     consoleInput.value = "";
 });
 
-// actually pauses console output
+    // actually pauses console output
 stopConsoleBtn.addEventListener("click", () => {
     if (isLogging) {
         console.log = consoleLog;
@@ -123,6 +117,7 @@ stopConsoleBtn.addEventListener("click", () => {
 
 });
 
+    //simple counter for console output
 testConsoleBtn.onclick = async () => {
     testConsoleBtn.disabled = true;
     for (let i = 0; i < 30; i++) {
@@ -131,20 +126,6 @@ testConsoleBtn.onclick = async () => {
     }
     testConsoleBtn.disabled = false;
 };
-
-
-toggleConsoleMultiline.addEventListener("click", () => {
-    if (isMultiline) {
-        isMultiline = false;
-        toggleConsoleMultiline.textContent = "multiline input";
-        consoleInput.placeholder = "Enter";
-    } else if (!isMultiline) {
-        isMultiline = true;
-        toggleConsoleMultiline.textContent = "single line input";
-        consoleInput.placeholder = "Ctrl + Enter";
-    }
-    
-});
 
 // change it
 let isSelecting = false;
